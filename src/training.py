ea27d8b914c56e8df6b58904cd610e89f1767056
Elie Sadaka <e.sdk.mail@gmail.com>
2025-09-14 18:23:14 +0900

    py for opencv

---
+++ src/training.py
@@ -0,0 +1,7 @@
+import cv2
+import os
+
+folder_path = "/home/es/Documents/Project/VG/Medias/Nintendo Famicom (2D Boxes)(Kondorito 1.2)"
+
+# List all files in the folder
+images = os.listdir(folder_path)
